7398110dd77eb3cb8cdaaaa74d596ea73c71bd56
menoc61 <gillemomeni@gmail.com>
2023-09-13 17:42:50 -0700

    order => delivery update

--- navigation/AppStack.js
+++ navigation/AppStack.js
@@ -1,14 +1,19 @@
 import * as React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
-
+import OrdersScreen from "../screens/OrdersScreen";
+import OrdersDeliveryScreen from "../screens/OrderDelivery";
 import { HomeScreen } from '../screens';
 
 const Stack = createStackNavigator();
 
 export const AppStack = () => {
   return (
-    <Stack.Navigator>
-      <Stack.Screen name='Home' component={HomeScreen} />
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="OrdersScreen" component={OrdersScreen} />
+      <Stack.Screen
+        name="OrdersDeliveryScreen"
+        component={OrdersDeliveryScreen}
+      />
     </Stack.Navigator>
   );
 };
